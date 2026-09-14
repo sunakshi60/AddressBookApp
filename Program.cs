5f531e51609789eda5884190e87f12809a936a61
Sunakshi <sunakshiisharma0060@gmail.com>
2026-09-14 13:05:05 +0530

    UC1: create Contact

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
-﻿
+﻿using AddressBookApp.Models;
+
 class Program
 {
     static void Main(string[] args)
     {
+        Contact contact = new Contact(
+        "John",
+        "Doe",
+        "12 MG Road",
+        "Pune",
+        "Maharashtra",
+        "411001",
+        "9876543210",
+        "john.doe@mail.com"
+        );
+
+        Console.WriteLine(contact);
+
+        Console.ReadLine();
     }
 }
